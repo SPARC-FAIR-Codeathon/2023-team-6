0d9f00cdec924cc39802e283c3b195a070bb3745
Mason Mings <mason.mings@payjoy.com>
2023-08-07 09:28:48 -0500

    pushing skeleton flask server file

---
+++ backend/server.py
@@ -0,0 +1,23 @@
+# Import flask and datetime module for showing date and time
+from flask import Flask
+import datetime
+ 
+x = datetime.datetime.now()
+ 
+# Initializing flask app
+app = Flask(__name__)
+ 
+ 
+# Post route placeholder for running script to generate and return dockerfile
+@app.route('/run')
+def run_script():
+ 
+    # Returning an api for showing in  reactjs
+    return {
+        'placeholder':"this is a placeholder"
+        }
+ 
+     
+# Running app
+if __name__ == '__main__':
+    app.run(debug=True)
